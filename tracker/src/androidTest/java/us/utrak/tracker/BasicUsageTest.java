3054f1c252136be574ea7a28c6231e493c7b0425
Thiago F Pappacena <pappacena@gmail.com>
2015-12-13 15:29:43 +0100

    adding another test

--- tracker/src/androidTest/java/us/utrak/tracker/BasicUsageTest.java
+++ tracker/src/androidTest/java/us/utrak/tracker/BasicUsageTest.java
@@ -3,6 +3,8 @@ package us.utrak.tracker;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import java.util.HashMap;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
@@ -11,9 +13,25 @@ public class BasicUsageTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
-    final public void testInitializeUTrakUs() throws Exception {
+    final public void testInitializeUTracker() throws Exception {
         UTracker tracker = UTrakUs.getInstance(getApplication()).getTracker("token1", "user1");
         assertEquals(tracker.getToken(), "token1");
         assertEquals(tracker.getUser(), "user1");
+    }
+
+    final public void testCreateEvent() {
+        Event e = new Event()
+                    .setMetric("redbutton_click")
+                    .setValue(1)
+                    .addTag("SO:Name", "Android")
+                    .addTag("SO:Version", "5.1");
+
+        assertEquals(e.getMetric(), "redbutton_click");
+        assertEquals(e.getValue(), 1.0);
+
+        HashMap<String, String> tags = e.getTags();
+        assertEquals(tags.size(), 2);
+        assertEquals(tags.get("SO:Name"), "Android");
+        assertEquals(tags.get("SO:Version"), "5.1");
     }
 }
